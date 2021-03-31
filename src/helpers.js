dba1b7b6d9259e70bba29061d8ef88b38c7740f2
Pierre A <pierre@avizou.eu>
2021-03-31 17:11:22 +0200

    Default expire time to 6 month

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-const getExpirationDate = (date = null, expiringDays = 365) => {
+const getExpirationDate = (date = null, expiringDays = 182) => {
   const expireDate = date ? new Date(date) : new Date();
   expireDate.setTime(expireDate.getTime() + (expiringDays * 24 * 60 * 60 * 1000));
   return expireDate;
